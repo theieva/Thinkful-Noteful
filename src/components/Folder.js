cb9b252cb3fec352d51d0235e24212faddf3a313
Alvin <alvinpl21@gmail.com>
2019-11-19 21:23:22 -0500

    first part done

---
+++ src/components/Folder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Folder extends React.Component {
+    render() {
+        return (
+            <div className='folder'>
+                <span>Folder 1</span>
+            </div>
+        )
+    }
+}
+
+export default Folder;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
+// import Note from './components/Note';
+import Folder from './Folder';
 
 class Main extends React.Component {
     render() {
         return (
             <>
                 <header>
-                    <Link to="/">Noteful</Link>
+                    <Link to="/"><h1 className='pageTitle'>Noteful</h1></Link>
                 </header>
-                <main>
-                    <h2>This is the main section.</h2>
-                </main>
-                <section class="sidebar">
-                    This is the sidebar.
+                <main id='main-container'>
+                 <section className='sidebar-column'>
+                       <Folder />
+                       <Folder />
+                       <Folder />
                 </section>
+                    <section className='main-column'>
+                        main
+                    </section>
+                   
+                </main>
             </>
         );
     }
